b4b9f5597d530609cd114a14d937ae613d9f0ed5
chick-p <chick.p.1120@gmail.com>
2021-04-09 15:39:47 +0900

    feat: implement to check comma

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,24 @@ import Source from "structured-source";
 
 export interface Options {
   periodMarks?: string[];
+  commaMarks?: string[];
 }
 
 const defaultOptions: Options = {
   periodMarks: ["。", "."],
+  commaMarks: ["、", ","],
 };
 
 const report: TextlintRuleModule<Options> = (context, options) => {
   const { Syntax, RuleError, report, getSource, fixer } = context;
+  const periodMarks = options?.periodMarks || defaultOptions.periodMarks;
+  const commaMarks = options?.commaMarks || defaultOptions.commaMarks;
+  const commaMarkRegExp = new RegExp(`[` + commaMarks!.join("") + `]`);
   return {
     [Syntax.Header](node) {
       const raw = getSource(node);
       const source = new Source(raw);
-      const resultForPeriod = checkEndsWithPeriod(
-        raw,
-        options || defaultOptions
-      );
+      const resultForPeriod = checkEndsWithPeriod(raw, { periodMarks });
       if (resultForPeriod.valid) {
         const index = source.positionToIndex({
           line: node.loc.start.line,
@@ -34,6 +36,27 @@ const report: TextlintRuleModule<Options> = (context, options) => {
         });
         report(node, ruleError);
       }
+      const resultForComma = commaMarkRegExp.exec(raw);
+      if (resultForComma && resultForComma.length > 0) {
+        const index = source.positionToIndex({
+          line: node.loc.start.line,
+          column: resultForComma.index,
+        });
+        const ruleError = new RuleError("Find comma", {
+          index,
+        });
+        report(node, ruleError);
+      }
+      // if (commaMarkRegExp.test(raw)) {
+      //   const index = source.positionToIndex({
+      //     line: node.loc.start.line,
+      //     column: resultForPeriod.index,
+      //   });
+      //   const ruleError = new RuleError("Ends with period", {
+      //     index,
+      //   });
+      //   report(node, ruleError);
+      // }
     },
   };
 };
